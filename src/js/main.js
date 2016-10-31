93a86008904add659435f4a04c70a3f0652860da
bengrunfeld <benjamin.grunfeld@gmail.com>
2016-10-30 17:11:05 -0700

    init

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// App Components
+var App = require('./components/app.js');
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('main')
+);
